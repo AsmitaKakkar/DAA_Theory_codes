979357b77e87f820a778a695e3109d63a20015a0
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-09-03 19:17:12 +0530

    --

--- insertionSort.c
+++ insertionSort.c
@@ -3,11 +3,42 @@
 
 int insertionSort(int arr[],int n)
 {
-	
+	int i,j,temp;
+	for(i=0;i<=n;i++)
+	{
+		temp=arr[i];
+		j=i-1;
+		while(j>=0 && arr[j]>temp)
+		{
+			arr[j+1]=arr[j];
+			j=j-1;
+		}
+		arr[j+1]=temp;
+	}
 }
 
 int main()
 {
+	int i,n,arr[100];
+	printf("Enter Limit!!\n");
+	scanf("%d",&n);
+	printf("Enter the elements in an array !!\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	printf("Unsorted array !!");
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+	insertionSort(arr,n);
+	printf("Sorted array !!\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+	
 	
 	return 0;
 }
